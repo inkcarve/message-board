6be2e69056742cf6f2d71b0ffeb86dfeb2a45827
Tway <tway@1-166-3-204.dynamic.hinet.net>
2016-05-15 14:36:05 +0800

    add JQuery Bootstrap sass-loader node-sass ...

--- build/shared/routes.js
+++ build/shared/routes.js
@@ -6,10 +6,6 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _react = require("react");
-
-var _react2 = _interopRequireDefault(_react);
-
 var _reactRouter = require("react-router");
 
 var _AppHandler = require("./components/AppHandler.jsx");
@@ -22,21 +18,20 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } //import { Route } from "react-router";
-
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 //createClass could not hot-load
-var App = _react2.default.createClass({
+var App = React.createClass({
   displayName: "App",
   render: function render() {
-    return _react2.default.createElement(
+    return React.createElement(
       "div",
       { className: "container" },
       this.props.children,
-      _react2.default.createElement(
+      React.createElement(
         "div",
         null,
-        _react2.default.createElement(
+        React.createElement(
           _reactRouter.Link,
           { to: "/about" },
           "About"
@@ -45,10 +40,10 @@ var App = _react2.default.createClass({
     );
   }
 });
-var Home = _react2.default.createClass({
+var Home = React.createClass({
   displayName: "Home",
   render: function render() {
-    return _react2.default.createElement(
+    return React.createElement(
       "h1",
       null,
       "Hello, world!test2"
@@ -71,7 +66,7 @@ var About = function (_React$Component) {
   _createClass(About, [{
     key: "render",
     value: function render() {
-      return _react2.default.createElement(
+      return React.createElement(
         "h2",
         null,
         "About"
@@ -80,7 +75,7 @@ var About = function (_React$Component) {
   }]);
 
   return About;
-}(_react2.default.Component);
+}(React.Component);
 
 ;
 
@@ -99,13 +94,13 @@ const routesConfig = {
 
 exports.default =
 // method 2: by react router
-_react2.default.createElement(
+React.createElement(
   _reactRouter.Router,
   { history: _reactRouter.browserHistory },
-  _react2.default.createElement(
+  React.createElement(
     _reactRouter.Route,
     { path: "/", component: App },
-    _react2.default.createElement(_reactRouter.IndexRoute, { component: _AppHandler2.default }),
-    _react2.default.createElement(_reactRouter.Route, { path: "about", component: About })
+    React.createElement(_reactRouter.IndexRoute, { component: _AppHandler2.default }),
+    React.createElement(_reactRouter.Route, { path: "about", component: About })
   )
 );
